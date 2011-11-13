6f11337d1506bda282f49e74eafa30a6ce0e89a1
alangiovannipc <alangiovannipc@gmail.com>
2011-11-13 05:54:25 -0500

    commit app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TdewCarpooling::Application.config.secret_token = 'e63fba2c7f1becf3c9c951361509907cfd95184064ffa0720a3212cc047ac42dfd403b4751d2e636dab69decd23d94cd529d0b57066c0676146f7339362fa1d2'
